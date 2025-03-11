e194bf3c8fece5d0b1fc8a9aab97029e11afcc2e
ctmittelstaedt <ctmittelstaedt@gmail.com>
2025-03-11 12:09:56 -0700

    updated plot

--- scripts/recognizer/call_quality_analysis.R
+++ scripts/recognizer/call_quality_analysis.R
@@ -3,28 +3,31 @@
 library(tidyverse)
 library(tidybayes)
 library(brms) 
+library(ggplot2)
 library(gghalves)
-install.packages("ggplot2")
+library(ggeffects)
+library(sjPlot)
+library(RColorBrewer)
+
 
 call_quality <- read_csv(file.path("data","recognizer","recognizer_call_quality_assessment.csv"))
 
-# Create a scatterplot matrix to check collinearity
-pairs(~ predict_score + harmonics + power_density, 
-      data=call_quality, main="Scatterplot Matrix for call quality data")
-
-# Double check
+# Check for collinearity
 cor(call_quality$harmonics,call_quality$power_density)
 
 # Set prior
-human_prior <- c(set_prior(prior = 'normal(4,8)', class='b', coef='harmonics'))	
+prior1 <- c(set_prior(prior = 'normal(4,6)', class='b', coef='harmonics'),
+            #set_prior(prior = 'normal(4,6)', class='b', coef='logharmonics'),
+                 set_prior(prior = 'normal(-13,6)', class='Intercept', coef=''))
 
 # Simple model - no power_density because collinearity issue
-quality_model_1  <- brm(predict_score ~ harmonics + log(harmonics),
+quality_model_1  <- brm(predict_score ~ harmonics,#+log(harmonics),
                        call_quality,
                        cores = 3,
                        chains = 3,
-                       iter = 4000,
-                       warmup = 1500)
+                       prior = prior1,
+                       iter = 5000,
+                       warmup = 2000)
 
 ## look at the distribution of the parameters, look at effective sample size ESS
 summary(quality_model_1)
@@ -35,25 +38,49 @@ plot(quality_model_1)
 ## plot a "goodness of fit" plot, compare your model distribution to the poster distriubtion of the data
 pp_check(quality_model_1)
 
+dummydt <- data.frame(harmonics = seq(from=0,to=9,length.out=20))
 
 ## create a data frame of predictions of our model
-predictions <- add_predicted_draws(call_quality,
-                                   quality_model_1,
-                                   re_formula = predict_score ~ log(harmonics))
+predictions <- add_epred_draws(call_quality,
+                              #dummydt,
+                              quality_model_1,
+                              re_formula = predict_score ~ harmonics,  #+log(harmonics),
+                              ndraws = 1000,
+                              seed=1
+                              )
 
-
+ggridges
 # Plot number of harmonics vs score
-ggplot(call_quality,aes(log(harmonics),predict_score))+
-  stat_lineribbon(data = predictions,aes(y = .prediction),.width = c(0.95), alpha = 0.25)+ ## 95% credible interval
-  stat_lineribbon(data = predictions,aes(y = .prediction),.width = c(0), alpha = 1)+ ## mean
-  geom_point()+
-  scale_color_viridis_d()+
-  scale_fill_viridis_d()+
-  theme_bw()+
-  labs(x= "Number of harmonics",y="Prediction score")
+ggplot(call_quality, aes(harmonics, predict_score)) +
+  stat_lineribbon(data = predictions, aes(y = .epred), .width = c(0.95), alpha = 0.25, fill = "#999933") +  # 95% credible interval
+  stat_lineribbon(data = predictions, aes(y = .epred), .width = c(0), alpha = 1, color = "#999933") +  # Mean prediction
+  geom_point(color = "#999933", size = 2.5, shape=16) +  # Adjust point size and shape
+  theme_bw() +  # Use a clean theme
+  labs(x = "Number of harmonics", y = "Prediction score") +
+  scale_x_continuous(breaks = seq(0, 9, 1), labels = seq(0, 9, 1)) +  # Ensure x-axis has integer values
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    legend.position = "none",
+    axis.title.x = element_text(size = 14, vjust = -1.5), 
+    axis.title.y = element_text(size = 14, vjust = 1.5),
+    axis.text.x = element_text(size = 12),
+    axis.text.y = element_text(size = 12),
+    panel.border = element_blank(), 
+    axis.line = element_line(color = "black", size = 0.5)
+  )
+
+#############################################
+
+# or could do this
+pred <- predict_response(
+  quality_model_1,
+  dummydt,
+  ci_level = 0.95,margin = "empirical",
+  )
+
+plot_model(quality_model_1,terms = "harmonics",type="pred")
 
 
-## results: this may or may not happen
-class(quality_model_1)
 
 
